a91a73036a3d93813b22614e3d8203494a4106ac
Davide131297 <chiffi.davide@gmx.de>
2024-03-17 19:10:37 +0100

    Revert "URL zeigt nicht mehr die ID sondern den Spieler Namen"
    This reverts commit cd271662da398863995700e8330e27318065c9ab.

--- src/Tabelle/TeilnehmerTabelle.js
+++ src/Tabelle/TeilnehmerTabelle.js
@@ -145,8 +145,8 @@ function TeilnehmerTabelle() {
         return { backgroundColor: 'transparent' };
     }
 
-    const handleClick = (name) => {
-        navigate(`/f1league/profil/${name}`);
+    const handleClick = (id) => {
+        navigate(`/f1league/profil/${id}`);
     }
 
     return (
@@ -187,7 +187,7 @@ function TeilnehmerTabelle() {
                         {personen.map((person, index) => (
                         <tr key={index}>
                             <td>{index + 1}</td> {/* Pos */}
-                            <td className='fahrer' onClick={() => handleClick(person.spielerID)}>{person.spielerID}</td> {/* Fahrer */}
+                            <td className='fahrer' onClick={() => handleClick(person.id)}>{person.spielerID}</td> {/* Fahrer */}
                             <td>{person.team}</td> {/* Konstrukteur */}
                             <td style={getCellStyle(person.bahrain)}> {/* Bahrain */}
                             <select 
