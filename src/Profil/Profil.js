2a80da1256f46d248f9789ae3182c2d0c3bc58e6
davidechiffi <davide.chiffi@iu-study.org>
2024-03-26 11:37:59 +0100

    Bewertung im Profil entfernt % Siege im Profil hinzugefügt

--- src/Profil/Profil.js
+++ src/Profil/Profil.js
@@ -5,11 +5,10 @@ import {
     Card,
     Image,
 } from '@mantine/core';
-import { Rating } from '@mantine/core';
 import classes from'./Profil.css';
 import f1helm from './f1helm.jpeg';
 import { db } from '../utils/firebase';
-import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 import Profiltabelle from './Profiltabelle';
 
 import AlfaRomeo from './../Teamlogos/AlfaRomeo.png';
@@ -26,21 +25,15 @@ import AlphaTauri from './../Teamlogos/AlphaTauri.png';
 const Profil = () => {
     let { id } = useParams();
     const [person, setPerson] = useState(null);
-    const [value, setValue] = useState(0);
-    const [angemeldeterUserID, setAngemeldeterUserID] = useState(null);
-    const [userRating, setUserRating] = useState(null);
+    const [siege, setSiege] = useState(0);
 
     const teamLogos = {
-      //  'Alfa Romeo': AlfaRomeo,
-      //  'Alpine': Alpine,
         'Aston Martin': AstonMartin,
         'Ferrari': Ferrari,
-       // 'Haas': Haas,
         'McLaren': McLaren,
         'Mercedes': Mercedes,
         'Red Bull': RedBull,
         'Williams': Williams,
-      //  'AlphaTauri': AlphaTauri
     };
 
     //Auslesen der Daten aus der Datenbank
@@ -51,65 +44,25 @@ const Profil = () => {
             if (docSnap.exists()) {
                 const personData = docSnap.data();
                 setPerson(personData);
-                setValue(personData.bewertung || 0); // Setzen den Wert auf person.bewertung, wenn es existiert, sonst auf 0
                 console.log("Document data:", personData);
+
+                // Berechnung der Siege
+                let siege = 0;
+                for (let key in personData) {
+                    if (key !== 'id' && key !== 'passwort' && key !== 'spielerID' && key !== 'team' && key !== 'gesamtPunkte') {
+                        if (personData[key] === 25 || personData[key] === 26) {
+                            siege++;
+                        }
+                    }
+                }
+                console.log("Anzahl der Siege:", siege);
+                setSiege(siege);
             } else {
                 console.log("Kein solches Dokument!");
             }
         };
         fetchData();
     }, [id]);
-
-    //Ausgabe der Bewertung
-    useEffect(() => {
-        console.log(value);
-    }, [value]);
-
-    //Aktualisierung der Bewertung in der Datenbank
-    useEffect(() => {
-        const updateRating = async () => {
-            if (userRating === null) return; // Führen Sie den Code nur aus, wenn der Benutzer eine Bewertung abgegeben hat
-    
-            const docRef = doc(db, 'personen', id);
-            const docSnap = await getDoc(docRef);
-    
-            if (docSnap.exists() && angemeldeterUserID && person) {
-                const data = docSnap.data();
-                const newRatings = {...(data.bewertungen || {}), [angemeldeterUserID]: userRating};
-                const averageRating = Object.values(newRatings).reduce((a, b) => a + b, 0) / Object.values(newRatings).length;
-    
-                await updateDoc(docRef, {
-                    bewertung: averageRating,
-                    bewertungen: newRatings
-                });
-    
-                const userDocRef = doc(db, 'personen', angemeldeterUserID);
-                await updateDoc(userDocRef, {
-                    [`bewertet.${person.spielerID}`]: userRating
-                });
-            }
-        };
-        updateRating();
-    }, [userRating, id, angemeldeterUserID, person]); // Abhängigkeit von userRating statt value
-
-    //Ausgabe der Bewertung aus der Datenbank
-    useEffect(() => {
-        if (person && person.bewertung) {
-            console.log(`Die Bewertung aus der Datenbank ist ${person.bewertung}.`);
-        }
-    }, [person]);
-
-    //Auslesen der ID des angemeldeten Users
-    useEffect(() => {
-        const cookies = document.cookie.split(';');
-        cookies.forEach(cookie => {
-            const [name, value] = cookie.split('=');
-            if (name.trim() === 'userID') {
-                setAngemeldeterUserID(value);
-                console.log(`Die ID des angemeldeten Users ist ${value}.`);
-            }
-        });
-    }, []);
 
     const teamColors = {
         'Ferrari': 'red',
@@ -142,25 +95,11 @@ const Profil = () => {
                             <div className='profildaten'>
                                 {person && <span id="spielername" style={{color: teamColors[person.team]}}>{person.spielerID}</span>}
                                 <br></br>
-                                {person && <span id="spielerinfos">Team: {person.team}</span>}
+                                {person && <span className="spielerinfos">Team: {person.team}</span>}
                                 <br></br>
-                                {person && <span id="spielerinfos">Punkte: {person.gesamtPunkte}</span>}
-                            </div>
-                        </Card.Section>
-
-                        <Card.Section className={classes.footer}>
-                            <div className='profildaten'>
-                                <span id='fahrerbewertung'>
-                                Fahrer bewerten
-                                </span>
-                                    <Rating value={value} onChange={(newValue) => {
-                                        if (angemeldeterUserID) {
-                                            setValue(newValue);
-                                            setUserRating(newValue); // Aktualisiert userRating, wenn der Benutzer eine Bewertung abgibt
-                                        } else {
-                                            alert('Bitte melde dich an, um eine Bewertung abzugeben.');
-                                        }
-                                    }} />
+                                {person && <span className="spielerinfos">Punkte: {person.gesamtPunkte}</span>}
+                                <br></br>
+                                {person && <span className="spielerinfos">Siege: {siege}</span>}
                             </div>
                         </Card.Section>
                     </Card>
